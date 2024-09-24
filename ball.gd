976cfce61886814426b6fe83c65cf8de45aa847d
Freaky Meal <122627515+FreakyMeal@users.noreply.github.com>
2024-09-24 19:57:27 +0200

    Début de l'UI

--- ball.gd
+++ ball.gd
@@ -5,9 +5,7 @@ var velocity := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position = Vector2(640, 360)  # Field center
-	velocity = Vector2(randf_range(-1, 1), randf_range(-0.1, 0.1)).normalized() # Ball starts with a random direction
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@export var player: PackedScene
+@export var player_scene: PackedScene
+@export var ball_scene: PackedScene
 
 var direction:Vector2 = Vector2(1,0)
 
@@ -9,6 +10,9 @@ const P2_START_POSITION:Vector2 = Vector2(1270,455)
 
 var p1_score: int = 0
 var p2_score: int = 0
+
+var ball_acceleration: int = 50
+
 
 func _ready() -> void:
 	reset()
@@ -20,16 +24,16 @@ func _process(_delta: float) -> void:
 
 func reset():
 	get_tree().call_group("players", "queue_free")
-	reset_players()
+	set_players()
 	reset_score()
 
-func reset_players():
-	var player1 = player.instantiate()
+func set_players():
+	var player1 = player_scene.instantiate()
 	player1.position = P1_START_POSITION
 	player1.up_key = "up_player_1"
 	player1.down_key = "down_player_1"
 	
-	var player2 = player.instantiate()
+	var player2 = player_scene.instantiate()
 	player2.position = P2_START_POSITION
 	player2.up_key = "up_player_2"
 	player2.down_key = "down_player_2"
@@ -37,12 +41,20 @@ func reset_players():
 	add_child(player1)
 	add_child(player2)
 
+
 func reset_score():
 	p1_score = 0
 	p2_score = 0
 
+func start_game():
+	var ball = ball_scene.instantiate()
+	ball.position = Vector2(640, 360)  # Field center
+	ball.velocity = Vector2(randf_range(-1, 1), randf_range(-0.1, 0.1)).normalized() # Ball starts with a random direction
+	add_child(ball)
+
 func _on_speed_check_body_entered(_body: Node2D) -> void:
-	$Ball.speed += 50
+	if is_instance_valid($Ball):
+		$Ball.speed += ball_acceleration
 
 
 func _on_left_goal_zone_body_entered(body: Node2D) -> void:
@@ -55,3 +67,8 @@ func _on_right_goal_zone_body_entered(body: Node2D) -> void:
 	if body == $Ball:
 		$Ball.queue_free()
 		p1_score += 1
+
+
+func _on_button_pressed() -> void:
+	start_game()
+	$StartButton.hide()
